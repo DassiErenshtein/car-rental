3a59e8be3908f7f9425f9ec1b9d8da1e4354a082
DassiErenshtein <hadasa2115@gmail.com>
2025-04-26 22:27:17 +0300

    add project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import {Main} from './car-rental/Main'
+import './App.css';
+
+function App() {
+  return (
+   <Main></Main>
+  );
+}
+
+export default App;
